f9e46b0a4a1ac8678819644a065f8c2898292bd1
technophage <g0r@technophage.net>
2021-09-14 13:56:56 +0100

    Update bb32.ino

--- bb32.ino
+++ bb32.ino
@@ -1,25 +1,23 @@
 /* 
- * Simple 32 button game controller (button box) w/ no encoders 
+ * Simple 32 button game controller (button box)
  *
  * Key Matrix;
- * Rows pin : A0,A1,A2,A3
- * Cols pin : 5,6,7,8,9,10,11,12
- *  
- * Developed & tested on ATmega32u4 5V 16MHZ compatible Arduino clone
+ *  Rows pin : A0,A1,A2,A3
+ *  Cols pin : 5,6,7,8,9,10,11,12
  * 
  * Required Librarys; 
+ *    Keypad    : https://playground.arduino.cc/Code/Keypad/
+ *    Joystick  : https://github.com/MHeironimus/ArduinoJoystickLibrary
  * 
- *    Keypad https://playground.arduino.cc/Code/Keypad/
- *    Joystick https://github.com/MHeironimus/ArduinoJoystickLibrary
+ * Developed & tested on ATmega32u4 5V 16MHZ compatible Arduino clone
  * 
  */
- 
 
-#include <Joystick.h>
+#include <Key.h>
 #include <Keypad.h>
+#include <Joystick.h>
 
 #define ENABLE_PULLUPS
-
 
 #define BUTTONS 32
 #define KMROWS 4
@@ -35,31 +33,29 @@ byte keyMatrix[KMROWS][KMCOLS] = {
   {24,25,26,27,28,29,30,31},
 };
 
-
-
 Keypad bb32 = Keypad(makeKeymap(keyMatrix), rowPins, colPins, KMROWS, KMCOLS); 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK, BUTTONS, 0, 
-                    false, false, false, false, false, false, 
-                    false, false, false, false, false);
+  false, false, false, false, false, false, 
+  false, false, false, false, false);
 
-void scanButtons(void) {
-      if (bb32.getKeys()) {
-       for (int n=0; n<LIST_MAX; n++) {
-           if ( bb32.key[n].stateChanged ) {
-            switch (bb32.key[n].kstate) {
-                    case HOLD:
-                              Joystick.setButton(bb32.key[n].kchar, 1);
-                              break;
-                    case IDLE:
-                              Joystick.setButton(bb32.key[n].kchar, 0);
-                              break;
-          }
+void scanButtons(void)  {
+  if (bb32.getKeys()) {
+    for (int n=0; n<LIST_MAX; n++)  {
+      if (bb32.key[n].stateChanged) {
+        switch (bb32.key[n].kstate) {
+          case HOLD:
+            Joystick.setButton(bb32.key[n].kchar, 1);
+            break;
+          case IDLE:
+            Joystick.setButton(bb32.key[n].kchar, 0);
+            break;
         }
       }
     }
   }
+}
 
-void setup() {
+void setup()  {
   Joystick.begin();
 }
 
